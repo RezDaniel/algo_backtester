4cd130a672250226aa4078ba7bedd3940181a5d6
Rez <rez@codeblock.io>
2023-07-22 17:17:42 +0700

    working on adding datetime_ny column to DataManager.df

--- backtesting/__main__.py
+++ backtesting/__main__.py
@@ -1,9 +1,15 @@
+# __main.py__
 from feature_engineering import data_wrangling as ds
 from feature_engineering.feature_creation import DataManager as dmgt
-from feature_engineering import feature_selection
+from feature_engineering import feature_selection as fs
 
-start_date = '2019-01-01 00:00:00'
-end_date = '2019-01-31 23:59:59'
-sliced_df = ds.slice_data('data/clean_data/cleaned_btc',
-                          start_date, end_date)
+# start_date = '2019-01-01 00:00:00'
+# end_date = '2019-01-31 23:59:59'
+# sliced_df = ds.slice_data('data/clean_data/cleaned_btc',
+#                           start_date, end_date)
+
+csv_path = 'data/test_data/cleaned_btc_jan23.csv'
+date_col = 'timestamp'
+output_csv = 'data/test_data/test_sigbar_on_index.csv'
+fs.generate_sigtime_orders(csv_path, date_col, output_csv)
 
--- backtesting/feature_engineering/feature_selection.py
+++ backtesting/feature_engineering/feature_selection.py
@@ -2,7 +2,7 @@
 from feature_creation import DataManager
 
 
-def generate_sigbar_orders(csv_path, date_col, output_filename):
+def generate_sigtime_orders(csv_path, date_col, output_csv):
     # Create an instance of the DataManager class and load the data from the CSV
     dmgt = DataManager(csv_path, date_col)
 
@@ -10,7 +10,8 @@ def generate_sigbar_orders(csv_path, date_col, output_filename):
 
     # Generate orders using the specified lookback period for sigtime in min.
     lookback = 60
-    dmgt.generate_orders(lookback, output_filename)
+    buffer = 2
+    dmgt.generate_orders(lookback, buffer, output_csv)
 
 
 
